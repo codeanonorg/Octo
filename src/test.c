4383b70753e1cef7b0891ef7a85bad74adfbe013
Arthur Correnson <arthur.correnson@gmail.com>
2020-04-23 16:48:21 +0200

    Python modules manager

--- src/test.c
+++ src/test.c
@@ -1,24 +1,44 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
-static int numargs = 0;
+/**
+ * @brief Numbers of python modules loaded
+ *
+ */
+static int modules_loaded = 0;
 
-/* Return the number of arguments of the application command line */
-static PyObject *emb_numargs(PyObject *self, PyObject *args) {
-  if (!PyArg_ParseTuple(args, ":numargs"))
+/**
+ * @brief Numbers of working python modules loaded
+ *
+ */
+static int working_modules_loaded = 0;
+
+/**
+ * @brief Simply say Hello to Octo from a python module
+ *
+ */
+static PyObject *octo_hello(PyObject *self, PyObject *args) {
+  if (!PyArg_ParseTuple(args, ":hello"))
     return NULL;
-  return PyLong_FromLong(numargs);
+  printf("a module says hello to octo !\n");
+  Py_RETURN_NONE;
 }
 
-static PyMethodDef EmbMethods[] = {
-    {"numargs", emb_numargs, METH_VARARGS,
-     "Return the number of arguments received by the process."},
+static PyMethodDef octo_methods[] = {
+    {"hello", octo_hello, METH_VARARGS, "Simply say hello."},
     {NULL, NULL, 0, NULL}};
 
-static PyModuleDef EmbModule = {
-    PyModuleDef_HEAD_INIT, "emb", NULL, -1, EmbMethods, NULL, NULL, NULL, NULL};
+static PyModuleDef octo_module = {PyModuleDef_HEAD_INIT,
+                                  "octo",
+                                  NULL,
+                                  -1,
+                                  octo_methods,
+                                  NULL,
+                                  NULL,
+                                  NULL,
+                                  NULL};
 
-static PyObject *PyInit_emb(void) { return PyModule_Create(&EmbModule); }
+static PyObject *PyInit_octo() { return PyModule_Create(&octo_module); }
 
 /**
  * @brief Executing functions from a python module
@@ -26,65 +46,52 @@ static PyObject *PyInit_emb(void) { return PyModule_Create(&EmbModule); }
 
 int main(int argc, char const *argv[]) {
 
-  PyObject *pName, *pModule, *pFunc;
-  PyObject *pArgs, *pValue;
-  int i;
+  PyObject *pName, *pModule, *pInit, *pRet;
 
-  if (argc < 3) {
-    fprintf(stderr, "Usage: %s pythonfile funcname [args][\n", argv[0]);
+  if (argc < 1) {
+    fprintf(stderr, "Usage: %s pythonfile\n", argv[0]);
     return 1;
   }
 
-  numargs = 10;
-  PyImport_AppendInittab("emb", &PyInit_emb);
-
+  PyImport_AppendInittab("octo", &PyInit_octo);
   Py_Initialize();
-  pName = PyUnicode_DecodeFSDefault(argv[1]);
 
-  pModule = PyImport_Import(pName);
-  Py_DECREF(pName);
+  for (int i = 1; i < argc; i++) {
+    pName = PyUnicode_DecodeFSDefault(argv[i]);
+    pModule = PyImport_Import(pName);
+    Py_DECREF(pName);
 
-  if (pModule != NULL) {
-    pFunc = PyObject_GetAttrString(pModule, argv[2]);
-    /* pFunc is a new reference */
-
-    if (pFunc && PyCallable_Check(pFunc)) {
-      pArgs = PyTuple_New(argc - 3);
-      for (i = 0; i < argc - 3; ++i) {
-        pValue = PyLong_FromLong(atoi(argv[i + 3]));
-        if (!pValue) {
-          Py_DECREF(pArgs);
+    if (pModule != NULL) {
+      printf("Successfully loaded the module [%s]\n", argv[i]);
+      modules_loaded++;
+      pInit = PyObject_GetAttrString(pModule, "init");
+      if (pInit && PyCallable_Check(pInit)) {
+        pRet = PyObject_CallObject(pInit, NULL);
+        if (pRet != NULL) {
+          Py_DECREF(pRet);
+        } else {
+          Py_DECREF(pInit);
           Py_DECREF(pModule);
-          fprintf(stderr, "Cannot convert argument\n");
-          return 1;
+          PyErr_Print();
+          fprintf(stderr, "Call to init failed\n");
         }
-        /* pValue reference stolen here: */
-        PyTuple_SetItem(pArgs, i, pValue);
-      }
-      pValue = PyObject_CallObject(pFunc, pArgs);
-      Py_DECREF(pArgs);
-      if (pValue != NULL) {
-        printf("Result of call: %ld\n", PyLong_AsLong(pValue));
-        Py_DECREF(pValue);
+        working_modules_loaded++;
       } else {
-        Py_DECREF(pFunc);
-        Py_DECREF(pModule);
-        PyErr_Print();
-        fprintf(stderr, "Call failed\n");
-        return 1;
+        if (PyErr_Occurred())
+          PyErr_Print();
+        fprintf(stderr, "Cannot find function \"init\"\n");
       }
+      Py_XDECREF(pInit);
+      Py_DECREF(pModule);
     } else {
-      if (PyErr_Occurred())
-        PyErr_Print();
-      fprintf(stderr, "Cannot find function \"%s\"\n", argv[2]);
+      PyErr_Print();
+      fprintf(stderr, "Failed to load \"%s\"\n", argv[1]);
     }
-    Py_XDECREF(pFunc);
-    Py_DECREF(pModule);
-  } else {
-    PyErr_Print();
-    fprintf(stderr, "Failed to load \"%s\"\n", argv[1]);
-    return 1;
   }
+
+  // Shut down the python interp
+  printf("[%d/%d] modules loaded\n", modules_loaded, argc - 1);
+  printf("[%d/%d] working modules\n", working_modules_loaded, modules_loaded);
   if (Py_FinalizeEx() < 0) {
     return 120;
   }
